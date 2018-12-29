f1a2a71591eeecd930901f08050db5b934fd0cda
Josh Cohen <joshco@gmail.com>
2018-12-29 06:01:57 -0500

    gem hell

--- lib/hero/gcivic/engine.rb
+++ lib/hero/gcivic/engine.rb
@@ -7,7 +7,7 @@ module Hero
       #isolate_namespace Gcivic
 
       root=config.root
-      add="#{root}/lib/"
+      add=DIR["#{root}/lib/"]
       puts "ALP #{root} adding #{add}"
       config.autoload_paths += add
       add=Dir["#{root}/app/**/"]
